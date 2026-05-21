1703cfd4f44d2a092224ca3b0ba0a339ff54cedb
KY <acma13@gmail.com>
2026-05-21 16:59:19 +0900

    일정관리-시작일,종료일 뒤 시간 제거

--- frontend/lib/screens/club_calendar_screen.dart
+++ frontend/lib/screens/club_calendar_screen.dart
@@ -515,15 +515,19 @@ class _ClubCalendarScreenState extends State<ClubCalendarScreen> {
   List<ArcherAppointment> _getDataSource(List<dynamic> serverData) {
     return serverData.map((item) {
       
-      // 🏹 DB에 저장된 4280191205 같은 숫자를 가져옵니다.
+      // DB에 저장된 4280191205 같은 숫자를 가져옵니다.
       // 만약 데이터가 String으로 오면 int.parse를 쓰고, 아니면 바로 형변환 합니다.
       int colorValue = int.tryParse(item['color'].toString()) ?? 4281358132; // 기본값(초록)
+
+      // 만약 날짜 뒤에 T00:00:00.000 같은 찌꺼기가 붙어있다면, 'T'를 기준으로 앞의 날짜(년-월-일)만 쏙 가공합니다.
+      String startDateStr = item['start_date'].toString().split('T')[0];
+      String endDateStr = item['end_date'].toString().split('T')[0];
 
       return ArcherAppointment(
         id: item['id'].toString(),
         subject: item['title'] ?? '제목 없음',
-        startTime: DateTime.parse(item['start_date']),
-        endTime: DateTime.parse(item['end_date']),
+        startTime: DateTime.parse(startDateStr),
+        endTime: DateTime.parse(endDateStr),
         location: item['location'] ?? '',
         manager: item['manager'] ?? '',
         notes: item['content'] ?? '',
